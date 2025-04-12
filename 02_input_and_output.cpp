3a031ccc0b5660e8c0078ef9e0fe9ff38b9033e3
andtsouris <andtsouris@gmail.com>
2025-04-12 10:16:34 +0200

    Adding the solution to 3 problems (1-3)

---
+++ 02_input_and_output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num1;
+    int num2;
+    int num3;
+    int sum;
+    cin >> num1 >> num2 >> num3;
+    sum = num1 + num2 + num3;
+    cout << sum << endl;
+    return 0;
+}
---
+++ 03_basic_data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int my_int;
+    long my_long;
+    char my_char;
+    float my_float;
+    double my_double;
+    
+    scanf("%d %ld %c %f %lf", &my_int, &my_long, &my_char, &my_float, &my_double);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", my_int, my_long, my_char, my_float, my_double);
+    return 0;
+}
